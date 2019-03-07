ab468a14bd258fe37c3618a84267a398e6ae466c
Jack Yeung <jyeung.se@gmail.com>
2019-03-06 19:35:53 -0500

    Added Pagination and Displaying of GIFs Fetched.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,125 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  console.log("Hello Giphy Lovers")
-
-  let giphyStorage = {}
-  const giphyContainer = document.getElementById('giphy-container')
-
-  const loadGiphys = () => {
-    giphyContainer.style.display = 'block'
-  }
-
-
-  const showImg = (src) => {
-    let img = document.createElement("img")
-    img.src = src
-    img.width = 250
-    img.height = 250
-    document.body.appendChild(img)
-  }
+  const portal = 'AqKfwVr9s1p2Dfo1k95y3w5N8P96eGoX&limit=50'
 
   const fetchGiphys = () => {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=AqKfwVr9s1p2Dfo1k95y3w5N8P96eGoX&limit=10')
+    fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${portal}`)
     .then(res => res.json())
     .then(json => {
+      let giphyStorage
       giphyStorage = json.data
-      console.log(giphyStorage)
-      giphyStorage.map((gif) => showImg(gif.images.original.url) && gifList.push(gif.images.orginal.url))
+      for (let i = 0; i < giphyStorage.length; i++) {
+
+        const modal = document.getElementById("myModal")
+        const img = document.getElementsByClassName("gif-image")
+        const modalImg = document.getElementById("img-item")
+        const clickedImg = document.getElementById(giphyStorage[i].title)
+
+        // console.log(giphyStorage[i].images.original.url);
+        console.log(giphyStorage[i].title);
+        img.onclick = () => {
+          let modalGif = document.createElement("img")
+          let modalGifParent = document.getElementsByClassName("modal-content")
+          modalGif.src = `${giphyStorage[i].images.original.url}`
+          modalGifParent.appendChild(modalGif)
+
+          modal.style.display = "block"
+          // modalImg.src = this.src
+        }
+
+        modal.onclick = () => {
+          modal.style.display = "none"
+        }
+
+        // let detail = `
+        //   <img class="gif-image" id="gif-${i+1}" src="${giphyStorage[i].images.original.url}">
+        // `
       }
-    )
+
+      let calcNumOfPages = () => {
+        return Math.ceil(giphyStorage.length / numGifsDisplayed)
+      }
+
+    let displayTenGifList = []
+    let currentPage = 1
+    let numGifsDisplayed = 10
+    let numOfPages = calcNumOfPages()
+
+    const firstPage = () => {
+      currentPage = 1
+      showList()
+    }
+
+    const nextPage = () => {
+      currentPage += 1
+      showList()
+    }
+
+    const previousPage = () => {
+      currentPage -= 1
+      showList()
+    }
+
+    const lastPage = () => {
+      currentPage = numOfPages
+      showList()
+    }
+
+    const showList = () => {
+      const modal = document.getElementById("myModal")
+      const img = document.getElementsByClassName("gif-image")
+
+      document.getElementById("giphy-list").innerHTML = ""
+
+      let start = ((currentPage - 1) * numGifsDisplayed)
+      let end = start + numGifsDisplayed
+
+      displayTenGifList = giphyStorage.map(gifData => `<img class="gif-image" id="${gifData.title}" src="${gifData.images.original.url}" onclick="document.getElementById('img-item').src='${gifData.images.original.url}'">`).slice(start, end)
+
+      createList()
+      statusCheck()
+    }
+
+    const createList = () => {
+      for (let i = 0; i < displayTenGifList.length; i++) {
+        // const img = document.getElementById(`gif-${i+1}`)
+        // img ? img.style.display = "none" : false
+        document.getElementById("giphy-list").innerHTML += displayTenGifList[i]
+      }
+    }
+
+    const firstButton = document.getElementById("first")
+    const previousButton = document.getElementById("previous")
+    const nextButton = document.getElementById("next")
+    const lastButton = document.getElementById("last")
+
+    const statusCheck = () => {
+      firstButton.disabled = currentPage == 1 ? true : false
+      previousButton.disabled = currentPage == 1 ? true : false
+      nextButton.disabled = currentPage == numOfPages ? true : false
+      lastButton.disabled = currentPage == numOfPages ? true : false
+    }
+
+    firstButton.onclick = () => {
+      firstPage()
+    }
+
+    previousButton.onclick = () => {
+      previousPage()
+    }
+
+    nextButton.onclick = () => {
+      nextPage()
+    }
+
+    lastButton.onclick = () => {
+      lastPage()
+    }
+
+    showList()
+    })
   }
 
-  // const fetchGiphys = () => {
-  //   fetch('http://api.giphy.com/v1/gifs/trending?api_key=AqKfwVr9s1p2Dfo1k95y3w5N8P96eGoX&limit=10')
-  //   .then(res => res.json())
-  //   .then(json => {
-  //     giphyStorage = json.data
-  //     console.log(giphyStorage)
-  //     }
-  //   )
-  // }
-
-  let gifList = new Array()
-  let pagelist = new Array()
-  let currentPage = 1
-  let numGifsDisplayed = 10
-  let numOfPages = 1
-
-  const calcNumOfPages = () => {
-    return Math.ceil(gifList.length / numGifsDisplayed)
-  }
-
+  
   fetchGiphys()
-  numOfPages = calcNumOfPages()
-  console.log(gifList)
-  console.log(numOfPages)
-
 })
